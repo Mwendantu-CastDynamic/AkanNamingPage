23686321db8e46042de10e9972f0bc28fb031973
Brian Muchunku <brauxer@gmail.com>
2021-09-05 23:26:59 +0300

    Declare the datatypes of all the variable parameters and their corresponding results provided placing each in its respective array collection category

---
+++ scripts.js
@@ -0,0 +1,21 @@
+//THE AKAN APPLICATION
+
+//Business Logic
+// 1. Declare the variable types of the name result options to be generated
+//    placing them inside an array according to thier corresponding gender.
+
+// The maleNames and femaleNames in the array are in an indexed order 
+// which corresponds to the day of the week designation for each name.
+
+const daysOfTheWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+const femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+
+const maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku', 'Yaw', 'Kofi','Kwame'];
+
+
+
+("imageSubmit").click(function(event){
+  event.preventDefault();
+});
+
